e93c067ab640f775d66cf3011d545947a1a06fff
dadondev <dadondev03@gmail.com>
2023-12-26 20:50:18 +0500

    diction

---
+++ api.js
@@ -0,0 +1,9 @@
+// let text = "word";
+// async function getResponse(text) {
+//   const request = await fetch(
+//     `https://api.dictionaryapi.dev/api/v2/entries/en/${text}`
+//   );
+//   const response = await request.json();
+//   console.log(response);
+// }
+// getResponse(text);
